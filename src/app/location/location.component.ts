5ea21501450ab83f7a9b22d18a3ba480cc5002ab
George Ciocan <george.ciocan21@gmail.com>
2020-10-25 22:17:26 +0100

    imports booking button functionality

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 import { destinations } from '../destinations'
 
 @Component({
@@ -9,14 +10,17 @@ import { destinations } from '../destinations'
 })
 export class LocationComponent implements OnInit {
   location: any
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cartLogic: CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-    console.log(params)
+    // console.log(params)
     // this.location = destinations[+params.get('name')];
     this.location = destinations.find(trip => trip.destination === params.get('name') )
     //replaced default angular asignment to return the the object in respect to the "destination" key, passed through the slug, and not through the obejcts index 
     });
+  }
+  bookTrip(location: {}) {
+    this.cartLogic.addToCart(location)
   }
 }
